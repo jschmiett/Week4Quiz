8025e6d8f35ba7a616e2915e2ec2ffcf41cd1a3a
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-22 13:24:30 -0500

    Adding additonal option buttons and trying to get verifyAnswer button working when answer button is clicked

--- javascript.js
+++ javascript.js
@@ -4,51 +4,59 @@ var gameProgress = 0
 
 var questionArray = [
     {
-        question:"1st Question",
-        options:["Option A", "Option B"],
+        question: "1st Question",
+        options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
     {
-        question:"1st Question",
-        options:["Option A", "Option B"],
+        question: "2nd Question",
+        options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
     {
-        question:"3rd Question",
-        options:["Option A", "Option B"],
+        question: "3rd Question",
+        options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
     {
-        question:"1st Question",
-        options:["Option A", "Option B"],
+        question: "4th Question",
+        options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
     {
-        question:"1st Question",
-        options:["Option A", "Option B"],
+        question: "5th Question",
+        options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
 ];
 
-function questionOrder () {
+function questionOrder() {
     document.getElementById("questions").textContent = questionArray[gameProgress].question;
+    // Putting option buttons on screen below question
     // document.getElementById('divA').textContent = 'This text is different!';
-    for (var i = 0; i< questionArray[gameProgress].options.length; i++){
+    for (var i = 0; i < questionArray[gameProgress].options.length; i++) {
         var button = document.createElement("button");
         button.textContent = questionArray[gameProgress].options[i];
-        document.getElementById("answerButton").append(button)
+        document.getElementById("answerButton").append(button);
     }
 
+
+
+}
+
+function verifyAnswer() {
+    console.log("answer button clicked")
 }
 
 console.log(questionArray[2].question)
 
 document.getElementById("start").addEventListener("click", start);
 
-function start () {
+function start() {
     console.log("Worked");
     document.getElementById("welcome").setAttribute("class", "hide");
     document.getElementById("quiz").classList.remove("hide");
     questionOrder();
-}
+    button.getElementById("answerButton").addEventListener("click", verifyAnswer);
 
+}
