79ca4ab180422558aba75707e569b5da7b27a227
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-24 09:48:26 -0500

    Getting coundown interval function running and displaying to website

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 // let start = document.querySelector(".start");
 
 var gameProgress = 0
+var timeLeft = 60;
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('timerEl');
 
 var questionArray = [
     {
@@ -19,44 +22,88 @@ var questionArray = [
         correctAnswer: "MongoDB, Express, React, Node"
     },
     {
-        question: "Which Values are False-y?",
-        options: ["False, 0, -0, On, "", '', ``, null, undefined, NaN, document.all", "False, 0, -0, On, -On, "", '', ``, null, undefined, NaN", "False, 0, -0, On, -On, "", '', ``, null, undefined, NaN, document.all", "False, 0, On, "", '', ``, null, undefined, NaN, document.all"],
-        correctAnswer: "False, 0, -0, On, "", '', ``, null, undefined, NaN, document.all"
-    },
-    {
         question: "5th Question",
         options: ["Option A", "Option B", "Option C", "Option D"],
         correctAnswer: "Option B"
     },
 ];
 
+function countdown() {
+    console.log("Countdown started");
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            endGame();
+        }
+    }, 1000);
+}
+
+
 function questionOrder() {
+    document.getElementById("answerButton").innerHTML = "";
     document.getElementById("questions").textContent = questionArray[gameProgress].question;
     // Putting option buttons on screen below question
     // document.getElementById('divA').textContent = 'This text is different!';
     for (var i = 0; i < questionArray[gameProgress].options.length; i++) {
         var button = document.createElement("button");
         button.textContent = questionArray[gameProgress].options[i];
+        button.onclick = verifyAnswer;
         document.getElementById("answerButton").append(button);
     }
-
-
-
 }
 
 function verifyAnswer() {
-    console.log("answer button clicked")
+    console.log("answer button clicked");
+    console.log(this.textContent);
+    if (this.textContent === questionArray[gameProgress].correctAnswer) {
+        console.log("correct")
+    } else {
+        console.log("incorrect");
+    }
+    gameProgress++
+    if (gameProgress > questionArray.length - 1) {
+        console.log("End Game");
+        endGame();
+    } else {
+        questionOrder();
+    }
 }
+
+function endGame() {
+    document.getElementById("quiz").classList.add("hide");
+    document.getElementById("endGame").classList.remove("hide");
+};
+
+document.getElementById("endGameSubmitButton").addEventListener("click", function () {
+    var initials = document.getElementById("initials").value;
+    console.log(initials);
+    // save player to local storage
+})
 
 console.log(questionArray[2].question)
 
 document.getElementById("start").addEventListener("click", start);
+document.getElementById('start').addEventListener("click", countdown);
 
 function start() {
     console.log("Worked");
     document.getElementById("welcome").setAttribute("class", "hide");
     document.getElementById("quiz").classList.remove("hide");
     questionOrder();
-    button.getElementById("answerButton").addEventListener("click", verifyAnswer);
-
 }
